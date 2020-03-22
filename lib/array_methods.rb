9fcbd3273c170049ca3ca3bbaf4fee5d47299bce
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-22 07:20:00 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do
+    if array.include?(value_to_find)
+      return p array.index(value_to_find)
+    else
+      return p nil
+    end
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  p array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  p array.min
 end
